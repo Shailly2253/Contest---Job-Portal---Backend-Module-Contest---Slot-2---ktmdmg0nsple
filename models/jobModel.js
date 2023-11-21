9d8f548206d7d60bc5d74b75f8e2ca54a8d3a24d
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-11-21 18:34:43 +0530

    Update jobModel.js

--- models/jobModel.js
+++ models/jobModel.js
@@ -2,10 +2,10 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-  // Field 1: Define a property for 'title' with a String data type
-  // Field 2: Define a property for 'company' with a String data type
-  // Field 3: Define a property for 'location' with a String data type
-  // Field 4: Define a property for 'description' with a String data type
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  location: { type: String },
+  description: { type: String },
 });
 
 const Job = mongoose.model('Job', jobSchema);
