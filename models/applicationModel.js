996d3499cdaa2db5efc9404f8d06c934bf496d12
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-11-21 18:35:01 +0530

    Update applicationModel.js

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -1,11 +1,12 @@
+// models/applicationModel.js
 const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema({
-  // Field 1: Define a property for 'job' with a reference to the 'Job' model
-  // Field 2: Define a property for 'applicantName' with a String data type
-  // Field 3: Define a property for 'applicantEmail' with a String data type
-  // Field 4: Define a property for 'coverLetter' with a String data type
-  // Field 5: Define a property for 'applicationDate' with a Date data type
+  job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job', required: true },
+  applicantName: { type: String, required: true },
+  applicantEmail: { type: String, required: true },
+  coverLetter: { type: String },
+  applicationDate: { type: Date, default: Date.now },
 });
 
 const Application = mongoose.model('Application', applicationSchema);
