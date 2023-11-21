2148b32ced1865eb4b6fa86f85bf7dfe3f4e0041
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-11-21 18:34:09 +0530

    Update jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,44 +1,48 @@
+// controllers/jobController.js
 const Job = require('../models/jobModel');
 const Application = require('../models/applicationModel');
 
-// Function to get all jobs
 exports.getJobs = async (req, res) => {
   try {
-    // TODO: Define the logic to retrieve all job listings from the database
-    // TODO: Respond with the list of job listings
+    const jobs = await Job.find();
+    res.status(200).json(jobs);
   } catch (error) {
-    // Handle errors by responding with a 500 Internal Server Error
     res.status(500).json({ error: 'Internal server error' });
   }
 };
 
-// Function to create a new job listing
 exports.createJob = async (req, res) => {
   const { title, company, location, description } = req.body;
 
   try {
-    // TODO: Define the logic to create a new job listing based on the provided data
-    // TODO: Save the new job listing to the database
-    // TODO: Respond with a success message and the newly created job listing
+    const newJob = new Job({ title, company, location, description });
+    const savedJob = await newJob.save();
+    res.status(201).json(savedJob);
   } catch (error) {
-    // Handle errors by responding with a 500 Internal Server Error
     res.status(500).json({ error: 'Internal server error' });
   }
 };
 
-// Function to apply for a job
 exports.applyForJob = async (req, res) => {
   const { jobId } = req.params;
   const { applicantName, applicantEmail, coverLetter } = req.body;
 
   try {
-    // TODO: Retrieve the job listing with the given 'jobId' from the database
-    // TODO: Check if the job listing exists and handle the case when it doesn't
-    // TODO: Create a new job application with the provided applicant information
-    // TODO: Save the new job application to the database
-    // TODO: Respond with a success message and the newly created job application
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({ error: 'Job not found' });
+    }
+
+    const newApplication = new Application({
+      job: jobId,
+      applicantName,
+      applicantEmail,
+      coverLetter,
+    });
+
+    const savedApplication = await newApplication.save();
+    res.status(201).json(savedApplication);
   } catch (error) {
-    // Handle errors by responding with a 500 Internal Server Error
     res.status(500).json({ error: 'Internal server error' });
   }
 };
